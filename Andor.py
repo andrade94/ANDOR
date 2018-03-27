59099e993ce98fad0bd7e1b8c70a4f6fb858b284
andrade94 <eduardo.mtz9@gmail.com>
2018-03-27 16:43:33 -0600

    Changes.

--- Andor.py
+++ Andor.py
@@ -131,210 +131,223 @@ start = 'PROGRAMA'
 
 # For using empty
 def p_empty(p):
-    '''empty :'''
+    '''empty : 
+    '''
     pass
 # listo
 def p_PROGRAMA(p):
-  '''PROGRAMA : PROGRAMAZ PROGRAMAB PRINCIPAL'''
+  '''PROGRAMA : PROGRAMAZ PROGRAMAB PRINCIPAL
+  '''
 # listo
 def p_PROGRAMAB(p):
-  '''PROGRAMAB : FUNCION PROGRAMAB
-    | empty'''
+  '''PROGRAMAB  : FUNCION PROGRAMAB
+                | empty
+  '''
 # listo
 def p_PROGRAMAZ(p):
-  '''PROGRAMAZ : GLOBALES
-  | empty'''
+  '''PROGRAMAZ  : GLOBALES
+                | empty
+  '''
 # listo
 def p_GLOBALES(p):
-  '''GLOBALES : GLOBAL VART'''
+  '''GLOBALES : GLOBAL GLOBALEZ END
+  '''
+def p_GLOBALEZ(p):
+  '''GLOBALEZ : VART GLOBALEZ
+              | empty
+  '''
 # listo
 def p_VART(p):
   '''VART : DRAW ID EQUAL NEW DRAWI LPAR RPAR
-  	| DATA_TIPOS ID VARZ
-  		| ARR ID'''
-# listo
-def p_VARZ(p):
-  '''VARZ : empty
-  	| EQUAL EXP'''
+  	      | DATA_TIPOS ID 
+  		    | ARR ID
+  '''
 # listo
 def p_ESTATUTO(p):
-  '''ESTATUTO : ASIGNACION 
-  | CONDICION
-  | CICLO
-  | ACCION
-  | VART
-  | LLAMADA_FUNCION
-  | IMPRIMIR'''
+  '''ESTATUTO : EXPRE
+              | CONDICION
+              | CICLO
+              | ACCION
+              | VART
+              | LLAMADA_FUNCION
+              | IMPRIMIR
+  '''
 # listo 
 def p_BLOQUE(p):
-  '''BLOQUE : ESTATUTO BLOQUEP'''
-# listo
-def p_BLOQUEP(p):
-  '''BLOQUEP : BLOQUE 
-  | empty'''
+  '''BLOQUE : ESTATUTO BLOQUE
+            | empty
+  '''
 # listo
 def p_DATA_TIPOS(p):
   '''DATA_TIPOS : INT
-  | BOOLEAN
-  | CHAR
-  | STRING
-  | FLOAT'''
+                | BOOLEAN
+                | CHAR
+                | STRING
+                | FLOAT
+                | VOID
+  '''
 # listo 
 def p_ASOP(p):
   '''ASOP : PLUS
-  | MINUS'''
+          | MINUS
+  '''
 # listo
 def p_MDOP(p):
   '''MDOP : MULT
-  | DIVI'''
-# listo
-# def p_LOGOP(p):
-#  '''LOGOP : AND
-#  | OR'''
-# listo
-def p_ASIGNACION(p):
-  '''ASIGNACION : ID ASIGNACIONP'''
-# listo
-def p_ASIGNACIONP(p):
-  '''ASIGNACIONP : EQUAL EXPRE 
-  | LBRA ICTE ASIGNACIONZ RBRA'''
-# listo
-def p_ASIGNACIONZ(p):
-  '''ASIGNACIONZ : COMMA ICTE
-  | empty'''
+          | DIVI
+  '''
 # listo
 def p_ARR(p):
-  '''ARR : DATA_TIPOS LBRA ICTE ARRZ RBRA'''
-# listo
-def p_ARRZ(p):
-  '''ARRZ : COMMA ICTE
-  | empty'''
+  '''ARR : DATA_TIPOS LBRA ICTE RBRA
+  '''
 # listo
 def p_LLAMADA_FUNCION(p):
-  '''LLAMADA_FUNCION : ID LPAR LLAMADA_FUNCIONP RPAR'''
+  '''LLAMADA_FUNCION : ID LPAR LLAMADA_FUNCIONP RPAR
+  '''
 # listo
 def p_LLAMADA_FUNCIONP(p):
-  '''LLAMADA_FUNCIONP : EXPRE LLAMADA_FUNCIONZ
-  | empty'''
+  '''LLAMADA_FUNCIONP : EXPRE LLAMADA_FUNCIONZ LLAMADA_FUNCIONP
+                      | empty
+  '''
 # listo
 def p_LLAMADA_FUNCIONZ(p):
-  '''LLAMADA_FUNCIONZ : COMMA LLAMADA_FUNCIONP
-  | empty'''
+  '''LLAMADA_FUNCIONZ : COMMA
+                      | empty
+  '''
 # listo
 def p_PRINCIPAL(p):
-  '''PRINCIPAL : DEFINE VOID MAIN LPAR RPAR BLOQUE END'''
+  '''PRINCIPAL : MAIN LPAR RPAR BLOQUE END
+  '''
 # listo
 def p_CICLO(p):
-  '''CICLO : WHILEF
-  | FORZ'''
+  '''CICLO  : WHILEF
+            | FORZ
+  '''
 # check
 def p_WHILEF(p):
-  '''WHILEF : WHILE LPAR EXPRE RPAR BLOQUE END'''
+  '''WHILEF : WHILE LPAR EXPRE RPAR BLOQUE END
+  '''
 # listo
 def p_FORZ(p):
-  '''FORZ : FOR LPAR ICTE COMMA ICTE COMMA ICTE RPAR BLOQUE END'''
+  '''FORZ : FOR LPAR ICTE COMMA ICTE COMMA ICTE RPAR BLOQUE END
+  '''
 # listo
 def p_CONDICION(p):
-  '''CONDICION : IF LPAR EXPRE RPAR BLOQUE CONDICIONP END'''
-# listo
+  '''CONDICION : IF LPAR EXPRE RPAR BLOQUE CONDICIONP END
+  '''
 def p_CONDICIONP(p):
-  '''CONDICIONP : ELSE CONDICIONZ
-  | empty'''
-# listo
-def p_CONDICIONZ(p):
-  '''CONDICIONZ : BLOQUE empty
-  | CONDICION'''
+  '''CONDICIONP : ELSE BLOQUE
+                | empty
+  '''
 # listo
 def p_RELOP(p):
-  '''RELOP : LESSTH 
-  | GREATERTH
-  | SEQUAL
-  | NOTEQ
-  | LESSEQTH
-  | GREATEREQTH'''
+  '''RELOP  : LESSTH 
+            | GREATERTH
+            | SEQUAL
+            | NOTEQ
+            | LESSEQTH
+            | GREATEREQTH
+  '''
 # listo
 def p_EXPRE(p):
-  '''EXPRE : EXP EXPREZ'''
-# listo
+  '''EXPRE : EXT EXPREZ
+  '''
 def p_EXPREZ(p):
-  '''EXPREZ : RELOP EXP 
-  | empty'''
+  '''EXPREZ : EQUAL EXPRE
+            | empty
+  '''
+def p_EXT(p):
+  '''EXT  : EXP EXT_W_RELOP
+  '''
+def p_EXT_W_RELOP(p):
+  '''EXT_W_RELOP  : RELOP EXT
+        | empty
+  '''
 # listo
 def p_EXP(p):
-  '''EXP : TERMINO EXP_W_SIGN'''
+  '''EXP : TERMINO EXP_W_SIGN
+  '''
 # listo
 def p_EXP_W_SIGN(p):
   '''EXP_W_SIGN : ASOP EXP
-  | empty'''
+                | empty
+  '''
 # listo
 def p_TERMINO(p):
-  '''TERMINO : FAC TERMINO_W_SIGN'''
+  '''TERMINO : FAC TERMINO_W_SIGN
+  '''
 # listo
 def p_TERMINO_W_SIGN(p):
   '''TERMINO_W_SIGN : MDOP TERMINO
-  | empty'''
+                    | empty
+  '''
 # listo
 def p_VAR_CTE(p):
-  '''VAR_CTE : ICTE
-  | FCTE
-  | LLAMADA_FUNCION'''
+  '''VAR_CTE  : ICTE
+              | FCTE
+  '''
 # listo  
 def p_IMPRIMIR(p):
-  '''IMPRIMIR : PRINT LBRA IMPRIMIRZ RBRA'''
+  '''IMPRIMIR : PRINT LBRA IMPRIMIRZ RBRA
+  '''
 # listo
 def p_IMPRIMIRZ(p):
-  '''IMPRIMIRZ : EXPRE 
-  | SCTE'''
+  '''IMPRIMIRZ  : EXPRE 
+                | SCTE
+  '''
 #listo
 def p_FAC(p):
-  '''FAC : LPAR EXPRE RPAR 
-  | FACS VAR_CTE
-  | ID FACT'''
+  '''FAC  : LPAR EXPRE RPAR
+          | VAR_CTE 
+          | LBRA EXPRE RPAR
+          | ID FACT
+  '''
   #listo
 def p_FACT(p):
-  '''FACT : LBRA EXP FACZ RBRA
-  | empty'''
-# listo
-def p_FACS(p):
-  '''FACS : ASOP
-  | empty'''
-# listo 
-def p_FACZ(p):
-  '''FACZ : COMMA EXP
-  | empty'''
+  '''FACT : LBRA EXPRE RBRA
+          | LPAR EXPRE RPAR
+          | empty
+  '''
 # listo
 def p_FUNCION(p): 
-  '''FUNCION : DEFINE DATA_TIPOS ID VAR_FUN BLOQUE RETURN EXP END'''
+  '''FUNCION : DEFINE DATA_TIPOS ID VAR_FUN BLOQUE RETURN EXPRE END
+  '''
 # listo
 def p_ACCION(p): 
-  '''ACCION : ID POINT DIBUJA LPAR VAR_CTE RPAR'''
+  '''ACCION : ID POINT DIBUJA LPAR VAR_CTE RPAR
+  '''
 # listo
 def p_VAR_FUN(p): 
-  ''' VAR_FUN : LPAR VAR_FUNP RPAR'''
+  ''' VAR_FUN : LPAR VAR_FUNP RPAR
+  '''
 # listo
 def p_VAR_FUNP(p): 
-  '''VAR_FUNP : DATA_TIPOS ID VAR_FUNZ'''
+  '''VAR_FUNP : DATA_TIPOS ID VAR_FUNZ
+              | empty
+  '''
 # listo
 def p_VAR_FUNZ(p): 
   '''VAR_FUNZ : COMMA VAR_FUNP
-  | empty'''
+              | empty
+  '''
 # listo
 def p_DIBUJA(p):
   '''DIBUJA : DEFINIRPOSICION
-  | DEFINIRCOLOR
-  | DERECHO
-  | REVERSA
-  | IZQUIERDA
-  | DERECHA
-  | VELOCIDAD
-  | BORRAR
-  | MOSTRAR
-  | OCULTAR
-  | CIRCULO
-  | DEFINIRX
-  | DEFINIRY
-  | ARCO
-  | GROSOR'''
+            | DEFINIRCOLOR
+            | DERECHO
+            | REVERSA
+            | IZQUIERDA
+            | DERECHA
+            | VELOCIDAD
+            | BORRAR
+            | MOSTRAR
+            | OCULTAR
+            | CIRCULO
+            | DEFINIRX
+            | DEFINIRY
+            | ARCO
+            | GROSOR
+  '''
 
 def p_error(p):
     if p:
