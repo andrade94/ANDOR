28a5a8f2ac13c046dfcff1c7c158e1b3af256f92
Julian Orozco <orozco.castorena@outlook.com>
2018-05-01 15:19:42 -0500

    changes in vm and quads

--- m_vm.py
+++ m_vm.py
@@ -3,12 +3,13 @@ import turtle
 
 class VirtualMachine:
     def __init__(self, filename):
-        self.instr_ptr = 0
-        self.instr_ptr_stack = []
-        self.function_call_stack = []
+        self.instr_ptr = 0  # Current quad
+        self.instr_ptr_stack = []   # Stack used when returning to previous instruction
+        self.function_call_stack = []   # Stack of function calls
+        self.return_dir = -1  # Address to return value
+        self.stack_dir = stack_address  # Address to start the stack
         self.obj = self.load_obj(filename)
         self.quads = self.obj["quads"]
-        #self.globals = self.obj["globals"]
         self.functions = self.obj["functions"]
         self.heap = {}
         self.mem = self.obj["mem"]
@@ -16,6 +17,18 @@ class VirtualMachine:
     def load_obj(self, filename):
         f = open(filename, "rb")
         return pickle.load(f)
+
+    # Adds current function variables to stack before calling another one
+    def save_state(self):
+        copy_dir = self.stack_dir
+        for item in self.mem.items()[:]:
+            if (item[0] >= self.stack_dir):
+                # copies the memory address
+                self.mem[copy_dir] = item[0]
+                #copies the value stored in that address
+                self.mem[copy_dir + 4] = item[1]
+                copy_dir += 8
+        self.stack_dir = copy_dir
 
     def run(self):
         #get first quad of the list
@@ -45,10 +58,17 @@ class VirtualMachine:
                     self.mem[res] = 1
                 else:
                     self.mem[res] = 0
+            elif(op == "return"):
+                self.mem[self.return_dir] = self.mem[op1]
             
             #functions operations
             if (op == "era"):
+                self.save_state()
                 self.function_call_stack.append(op1)
+                # checks if function returns a value
+                if (res):
+                    self.mem[res] = 'r'
+                    self.return_dir = res
                 for var in self.functions[op1][4]:
                     self.mem[var[0]] = var[1]
             if (op == "param"):
@@ -62,9 +82,14 @@ class VirtualMachine:
             # operators that change the instruction pointer
             if(op == "goto"):
                 self.instr_ptr = res
+            elif(op == "gotoFalse"):
+                if(self.mem[op1] == 0):
+                    self.instr_ptr = res
+                else:
+                    self.instr_ptr += 1
             elif(op == "gosub"):
                 self.instr_ptr_stack.append(self.instr_ptr + 1)
-                self.instr_ptr = self.functions[op1][2]
+                self.instr_ptr = res
             elif(op == "end"):
                 self.instr_ptr = self.instr_ptr_stack.pop()
             else:
